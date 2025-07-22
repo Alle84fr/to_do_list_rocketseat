d32cc21ee1963f9cae45a947cd3f21b85b313fa4
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-22 13:45:16 -0300

    add aula 10 Repository add img bd

---
+++ to_do_list/src/main/java/br/com/afr8799/to_do_list/users/IUserRepository.java
@@ -0,0 +1,11 @@
+package br.com.afr8799.to_do_list.users;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    // representação dos métodos
+
+
+}
