4f13a19b6c6aa1029c29a2b05ec0de98408a27d6
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-22 14:04:50 -0300

    add aula 11 >> Validando username

--- to_do_list/src/main/java/br/com/afr8799/to_do_list/users/IUserRepository.java
+++ to_do_list/src/main/java/br/com/afr8799/to_do_list/users/IUserRepository.java
@@ -4,8 +4,11 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface IUserRepository extends JpaRepository<UserModel, UUID>{
     // representação dos métodos
+
+    UserModel findByUsername(String username);
 
 
 }
