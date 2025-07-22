048d0aaf3176716314049c713b4b892d2ae3bc4d
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-22 18:57:41 -0300

    add aula 20 setando atributo na requisição

--- to_do_list/src/main/java/br/com/afr8799/to_do_list/task/TaskController.java
+++ to_do_list/src/main/java/br/com/afr8799/to_do_list/task/TaskController.java
@@ -1,10 +1,15 @@
 package br.com.afr8799.to_do_list.task;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 
 
 
@@ -16,9 +21,11 @@ public class TaskController {
     private ITaskRepository taskRepository;
     
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
-        System.out.println("Entrou no Controller");
+        System.out.println("Entrou no Controller ");
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
         var task = this.taskRepository.save(taskModel);
         return task;
     }
