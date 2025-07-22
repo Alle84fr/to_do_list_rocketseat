c816e4a47ce28d68537e098480c7c88a0d7f446d
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-22 16:17:55 -0300

    add aula 15 Cadastro de tarefas

---
+++ to_do_list/src/main/java/br/com/afr8799/to_do_list/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.afr8799.to_do_list.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
+    
+}
