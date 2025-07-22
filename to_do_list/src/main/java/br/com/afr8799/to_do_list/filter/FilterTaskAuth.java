899de4a39c581970128b0197472ef980f4b2ec45
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-22 17:19:25 -0300

    add aula 17 continuando implementação do filtro

--- to_do_list/src/main/java/br/com/afr8799/to_do_list/filter/FilterTaskAuth.java
+++ to_do_list/src/main/java/br/com/afr8799/to_do_list/filter/FilterTaskAuth.java
@@ -1,25 +1,50 @@
 package br.com.afr8799.to_do_list.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 // em FilterTaskAuth dar ctrl . escolher add unimplemented methods
-public class FilterTaskAuth implements Filter{
+public class FilterTaskAuth extends OncePerRequestFilter{
+
+
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+   
+        var Authorization = request.getHeader("Authorization");
 
-        System.out.println("Primeiro vem no filtro");
-        chain.doFilter(request, response);
+        //decod
+        var authEncoded = Authorization.substring("Basic".length()).trim();
+        System.out.println(authEncoded);
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+        System.out.println(authDecode);
+
+        var authString = new String(authDecode);
+        System.out.println(authString);
+
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
+
+        filterChain.doFilter(request, response);
     }
+
+
     
 }
