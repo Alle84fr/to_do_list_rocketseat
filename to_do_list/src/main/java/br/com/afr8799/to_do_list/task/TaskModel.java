ce6b9308f62a37ec3c1a1727fa14457118459307
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-23 12:22:48 -0300

    add aula 26 Try catch controle de caracteres

--- to_do_list/src/main/java/br/com/afr8799/to_do_list/task/TaskModel.java
+++ to_do_list/src/main/java/br/com/afr8799/to_do_list/task/TaskModel.java
@@ -30,5 +30,12 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createAt;
     
+    public void setTitle(String title) throws Exception{
+
+        if(title.length()>50) {
+            throw new Exception("Title deve conter no máx 50 caracteres");
+        }
+        this.title = title;
+    }
 
 }
